445d04340d2d41d8894960c8adbad38cba43a231
Karuppiah Natarajan <karuppiah7890@gmail.com>
2020-01-16 00:09:13 +0530

    add feature to send request body to kafka to test topic

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/karuppiah7890/kong-plugin-kafka-upstream
 
 go 1.13
 
-require github.com/karuppiah7890/go-pdk v0.0.0-20200105135504-77a4ed705c08
+require (
+	github.com/Shopify/sarama v1.25.0
+	github.com/eapache/go-resiliency v1.2.0 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/jcmturner/gofork v1.0.0 // indirect
+	github.com/karuppiah7890/go-pdk v0.0.0-20200105135504-77a4ed705c08
+	github.com/pierrec/lz4 v2.4.0+incompatible // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20190826022208-cac0b30c2563 // indirect
+	golang.org/x/crypto v0.0.0-20200115085410-6d4e4cb37c7d // indirect
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+	gopkg.in/jcmturner/gokrb5.v7 v7.4.0 // indirect
+)
--- kafka_upstream.go
+++ kafka_upstream.go
@@ -1,18 +1,77 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/Shopify/sarama"
 	"github.com/karuppiah7890/go-pdk"
+	"log"
 )
 
-type PluginConfig struct {}
-
-func New() interface{} {
-	return &PluginConfig{}
+type PluginConfig struct {
+	Producer sarama.SyncProducer
 }
 
-func (conf *PluginConfig) Access (kong *pdk.PDK) {
-	err := kong.Response.Exit(200, map[string]interface{}{"message": "you are da best! :D"}, nil)
+var Version = "0.1.0"
+
+func New() interface{} {
+	brokerList := []string{"localhost:9092"}
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll // Wait for all in-sync replicas to ack the message
+	config.Producer.Retry.Max = 10                   // Retry up to 10 times to produce the message
+	config.Producer.Return.Successes = true
+	producer, err := sarama.NewSyncProducer(brokerList, config)
 	if err != nil {
+		log.Fatalln("Failed to start Sarama producer:", err)
+	}
+	return &PluginConfig{Producer: producer}
+}
+
+func (jsonEncoder JSONEncoder) Encode() ([]byte, error) {
+	jsonData, err := json.Marshal(map[string]interface{}{"body": jsonEncoder})
+	if err != nil {
+		return nil, err
+	}
+	return jsonData, nil
+}
+
+type JSONEncoder string
+
+func (jsonEncoder JSONEncoder) Length() int {
+	data, err := jsonEncoder.Encode()
+	if err != nil {
+		return 0
+	}
+	return len(data)
+}
+
+func (conf *PluginConfig) Access(kong *pdk.PDK) {
+	body, err := kong.Request.GetRawBody()
+	if err != nil {
+		sendError(kong, err)
+		return
+	}
+
+	_, _, err = conf.Producer.SendMessage(&sarama.ProducerMessage{
+		Topic: "test",
+		Value: JSONEncoder(body),
+	})
+	if err != nil {
+		sendError(kong, err)
+		return
+	}
+
+	err = kong.Response.Exit(200, map[string]interface{}{"message": "message sent"}, nil)
+	if err != nil {
+		_ = kong.Log.Err(err.Error())
+	}
+}
+
+func sendError(kong *pdk.PDK, err error) {
+	exitErr := kong.Response.Exit(200, map[string]interface{}{
+		"message": "message not sent :/",
+		"error":   err.Error(),
+	}, nil)
+	if exitErr != nil {
 		_ = kong.Log.Err(err.Error())
 	}
 }
